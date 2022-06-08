29c3a2c60e2ed7524c4f016e4c531e0dc9304695
Vera Khomenko <pochtavkh@gmail.com>
2022-06-08 18:28:47 +0300

    add modal
    modal military

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,11 +1,27 @@
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[military-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//   }
+// })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn1: document.querySelector('[military-modal-open-1]'),
+    openModalBtn2: document.querySelector('[military-modal-open-2]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
