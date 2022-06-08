d6e235e531dc694809b1609e9e0ef70ae0dd306b
Vera Khomenko <pochtavkh@gmail.com>
2022-06-08 18:30:19 +0300

    Updates

---
+++ projectMilitaryAssistance.4d0fb0dd.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn1:document.querySelector("[military-modal-open-1]"),openModalBtn2:document.querySelector("[military-modal-open-2]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn1.addEventListener("click",o),e.openModalBtn2.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=projectMilitaryAssistance.4d0fb0dd.js.map
